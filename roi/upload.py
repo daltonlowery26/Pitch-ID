90f5bcb9d32e87e93e938c5572635522c7d5738c
Dalton Lowery 26 <daltonlowery1@outlook.com>
2025-07-24 14:51:30 -0500

    region of intrest modeling

---
+++ roi/upload.py
@@ -0,0 +1,22 @@
+import roboflow
+import os
+from dotenv import load_dotenv
+
+# dot env
+load_dotenv()
+
+# api key
+robo_api = os.getenv("ROBO_API")
+
+# data and paths
+rf = roboflow.Roboflow(api_key=robo_api)
+project = rf.workspace("fracture-pcnbk")
+hitter_path = "C:/Users/dalto/OneDrive/Pictures/Documents/Projects/Coding Projects/Pitch ID Model/data/roi_frames/"
+
+# upload data
+project.upload_dataset(
+    dataset_path = hitter_path, 
+    project_name= "pitch-id-roi-hts2i",
+    num_workers=15
+)
+
